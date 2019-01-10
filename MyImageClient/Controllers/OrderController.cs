44b5fdc0cf5840ca72de0b94fcf0dce8e50c228d
jamin2mk <jamin.2mk@gmail.com>
2019-01-10 15:43:55 +0700

    Init allthing

---
+++ MyImageClient/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyImageClient.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /* 
+         * [Order Step 1]
+         * Action for uploading the images
+         */        
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        /*
+         * [Order Step 2]
+         * Action for choosing the printing size and number of copies
+         */         
+        public ActionResult ChooseImageDetails()
+        {
+            return View();
+        }
+
+        /*
+         * [Order Step 3]
+         * Action for getting the shiping location and date of delivery
+         */       
+        public ActionResult GetShipInfo()
+        {
+            return View();
+        }
+
+        /*
+         * [Order Step 4]
+         * Action for adding the order to Cart
+         */        
+        public ActionResult AddToCart()
+        {
+            return View();
+        }
+
+        /*
+         * [Order Step 5]
+         * Action for geting the payment mode
+         */
+        public ActionResult Payment()
+        {
+            return View();
+        }
+
+
+    }
+}
